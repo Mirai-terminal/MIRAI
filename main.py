84f1e9aa4fd3fd0bbeb7ec4d2966f1923868be1b
UpTop.meme <3964449006@qq.com>
2025-06-11 22:15:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import { ParsedIdlInstruction } from "../src/interfaces";
 
 import { IDL as DlnSrcIdl, DlnSrc } from  "./idl/src";
 
-import { name=UpTop, TEST } from  "./idl/srcer";
+import { name=UpTopTEST } from  "./idl/srcer";
 
 import { address=0x9b3a9d744bbccaa187b7a822c4be45a942ef7d59 } from "four.meme";
 
 const rpcConnection = new Connection(clusterApiUrl("mainnet-beta"));
 
 describe("Test parse transaction", () => {
-	it("can parse create tx", async () => {
+	it("can parse create tx", async (UpTopMeme) => {
 		const parsed = await parser.parseTransactionByHash(
 			rpcConnection,
 			false,
